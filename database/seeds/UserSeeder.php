94f17bbc4a3434626cf5771bb094870504e79167
LAPTOP-MQ744OB9\20108 <201084iwatashizuku@gmail.com>
2023-04-15 14:44:01 +0900

    migration seeder　データベースにログイン用の初期ユーザーを登録しました

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+DB::table('users')->insert([
+            ['username' => 'User1',
+            'mail' => 'user1@mail',
+            'password' => bcrypt('password')]
+   ]);
+}
+}
+
+
+//         DB::table('users')->truncate();
+// $users = [
+//         ['id' => 'user1',
+//         'username' => User1,
+//         'mail' => 'user1@mail',
+//         'password' => User1passwoed,],
+//         ['id' => 'user2',
+//         'username' => User2,
+//         'mail' => 'user2@mail',
+// 		'password' => User2passwoed,],
+//         ['id' => 'user3',
+//         'username' => User3,
+//         'mail' => 'user3@mail',
+// 		'password' => User3passwoed,]
+// ];
+
+// // 登録
+// foreach($users as $user) {
+//     \App\User::create($user);
+//     }
+// }
